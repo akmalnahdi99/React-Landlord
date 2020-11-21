2768406b4d41ddeb874ce59407772c6be52bd41d
ayhammymy <ayhamsysy@gmail.com>
2020-11-22 00:07:52 +0800

    Add Filtered LandLord Posts Functionality

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -1,10 +1,12 @@
 import React from "react";
- 
 import PostCard from "./PostCard";
+import { AppContext } from "./../context/settings";
+
 //import postIcon from "img/money-bag.svg";
 
-const UserPosts = (props) => {
-  //  const posts = { ...props };
+const UserPosts = (props, filterBy) => {
+  const appContext = React.useContext(AppContext);
+  const activeFilter = appContext.settings.postsFilter;
 
   // Data will come from an api Later
   const posts = [
@@ -29,8 +31,7 @@ const UserPosts = (props) => {
     {
       id: 3,
       postTitle: "Post Title",
-      postBody:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque obcaecati magnam maxime fugit error voluptatem placeat nesciunt assumenda eveniet consectetur amet ratione dolore id perferendis suscipit eum, ut neque.",
+      postBody: "Lorem ipsum dolor, sit amet consectetur adipisicing e .",
       postIcon: "img/money-bag.svg",
       postDateTime: "17:50 | 25 Dec 2020",
       postType: "offers",
@@ -59,13 +60,22 @@ const UserPosts = (props) => {
     },
   ];
 
-  return (
-    <div id="vertical-timeline" className="vertical-container light-timeline searchable">
-      {posts.map((postItem) => {
-        return <PostCard key={postItem.id} {...postItem} />;
-      })}
-    </div>
-  );
+  var filteredPosts = posts;
+  if (activeFilter != "all") {
+    filteredPosts = posts.filter((item) => item.postType.toLowerCase() === activeFilter.toLowerCase());
+  }
+
+  if (filteredPosts.length > 0) {
+    return (
+      <div id="vertical-timeline" className="vertical-container light-timeline searchable">
+        {filteredPosts.map((postItem) => {
+          return <PostCard key={postItem.id} {...postItem} />;
+        })}
+      </div>
+    );
+  } else {
+    return <h2 style={{ textAlign: "center" }}>No Posts found !!!</h2>;
+  }
 };
 
 export default UserPosts;
