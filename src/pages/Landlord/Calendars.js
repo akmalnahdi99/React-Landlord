c8625926ae84422393f192b3f8e5f9ae516c9f45
akmalnahdi99 <73809811+akmalnahdi99@users.noreply.github.com>
2020-12-01 16:53:16 +0800

    images adjustment

--- src/pages/Landlord/Calendars.js
+++ src/pages/Landlord/Calendars.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import Footer from "../../components/static/Footer";
+import Header from "../../components/Header";
+import SiteMap from "../../components/SiteMap";
+import Callendar from "../../components/Callendar";
 
 const Calendars = () => {
     return (
-        <div>
+        <div id="page-wrapper" className="gray-bg" style={{ border: "0px solid red" }}>
+            <div className="border-bottom white-bg">
+                <div className="container-fluid">
+                <Header />
+                </div>
+            </div>
+
+            <div className="wrapper border-bottom page-heading bg-white">
+                <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-12">
+                    <SiteMap title="Check In &amp; Check Out" />
+                    </div>
+                </div>
+                </div>
+            </div>
             
+            <div className="wrapper border-bottom page-heading bg-white">
+                <div className="container-fluid">
+                    <div className="ibox">
+                        <Callendar/>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
         </div>
+
+
     )
 }
 
