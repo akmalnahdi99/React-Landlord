7b9b77899e7104676d20e236c7ba4f17c84f7efe
ayhammymy <ayhamsysy@gmail.com>
2020-12-03 23:32:54 +0800

    add qhyrun merge

--- src/pages/Landlord/Calendars.js
+++ src/pages/Landlord/Calendars.js
@@ -1,40 +1,37 @@
-import React from 'react'
+import React from "react";
 import Footer from "../../components/static/Footer";
 import Header from "../../components/Header";
 import SiteMap from "../../components/SiteMap";
-import CalendarPage from '../../components/CalendarPage.js';
-
+import CalendarPage from "../../components/CalendarPage.js";
 
 const Calendars = () => {
-    return (
-        <div id="page-wrapper" className="gray-bg" style={{ border: "0px solid red" }}>
-            <div className="border-bottom white-bg">
-                <div className="container-fluid">
-                <Header />
-                </div>
-            </div>
-
-            <div className="wrapper border-bottom page-heading bg-white">
-                <div className="container-fluid">
-                <div className="row">
-                    <div className="col-lg-12">
-                    <SiteMap title="Check In &amp; Check Out" />
-                    </div>
-                </div>
-                </div>
-            </div>
-            
-            <div className="wrapper wrapper-content animated fadeInRight py-5 pb-5">
-                <div className="container-fluid pb-5">
-                    <CalendarPage />
-                </div>
-            </div>
-
-            <Footer />
+  return (
+    <div id="page-wrapper" className="gray-bg" style={{ border: "0px solid red" }}>
+      <div className="border-bottom white-bg">
+        <div className="container-fluid">
+          <Header />
         </div>
+      </div>
 
+      <div className="wrapper border-bottom page-heading bg-white">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12">
+              <SiteMap title="Check In &amp; Check Out" />
+            </div>
+          </div>
+        </div>
+      </div>
 
-    )
-}
+      <div className="wrapper wrapper-content animated fadeInRight py-5 pb-5">
+        <div className="container-fluid pb-5">
+          <CalendarPage />
+        </div>
+      </div>
 
-export default Calendars
+      <Footer />
+    </div>
+  );
+};
+
+export default Calendars;
--- src/pages/Landlord/Financials.js
+++ src/pages/Landlord/Financials.js
@@ -1,19 +1,18 @@
-//under review
-import React from 'react'
+import React from "react";
 import Footer from "../components/static/Footer";
 import FinancialChart from "../../components/FinancialChart";
 
 const Financials = () => {
-    return (
-        <div class="wrapper wrapper-content animated fadeInRight">
-        <div class="container container-xs">
-          <div class="ibox ">
-              <FinancialChart title="Financials" />
-          </div>
+  return (
+    <div class="wrapper wrapper-content animated fadeInRight">
+      <div class="container container-xs">
+        <div class="ibox ">
+          <FinancialChart title="Financials" />
         </div>
-      <Footer />
       </div>
-    )
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default Financials
+export default Financials;
