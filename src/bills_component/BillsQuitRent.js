03c204884a70d8c9437c2c3f6166e43153b5a201
Shihab1994 <73818104+Shihab1994@users.noreply.github.com>
2020-12-04 13:07:27 +0800

    Bills container done

--- src/bills_component/BillsQuitRent.js
+++ src/bills_component/BillsQuitRent.js
@@ -9,6 +9,8 @@ export default function BillsQuitRent() {
   return (
       <div class="wrapper wrapper-content animated fadeInRight">
         <div class="container container-xs">
+        <div className="row justify-content-center">
+            <div className="col-lg-9 mb-3">
           <div class="ibox ">
               <QuitRentDetails title="Quit Rent" />
           </div>
@@ -18,6 +20,8 @@ export default function BillsQuitRent() {
         <div class="ibox">
           <Invoice2 title="Invoice" />
         </div>
+        </div>
+      </div>
       </div>
       <Footer />
     </div>
