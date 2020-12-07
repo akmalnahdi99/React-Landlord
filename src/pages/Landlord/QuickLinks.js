3b07cc76149a1a4d58a49fd18d2be6250f1d05f0
qhairun-jann <73168030+qhairun-jann@users.noreply.github.com>
2020-12-07 11:52:09 +0800

    alt added

--- src/pages/Landlord/QuickLinks.js
+++ src/pages/Landlord/QuickLinks.js
@@ -36,84 +36,84 @@ const QuickLinks = () => {
                             <div class="row text-left">
                                 <div class="col-12 align-self-center mt-4">
                                     <Link to="BillsServiceCharge" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/money-bag.svg" width="30px" />
+                                        <img src="/imgs/money-bag.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Service Charge
                                         </span>
                                     </Link>
                                     <Link to="bills-insurance.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/insurance.svg" width="30px" />
+                                        <img src="/imgs/insurance.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Insurance
                                         </span>
                                     </Link>
                                     <Link to="bills-quitrent.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/quitrent.svg" width="30px" />
+                                        <img src="/imgs/quitrent.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Quit Rent
                                         </span>
                                     </Link>
                                     <Link to="bills-assessmentrate.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/assessment.svg" width="30px" />
+                                        <img src="/imgs/assessment.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Assessment Rate
                                         </span>
                                     </Link>
                                     <Link to="bills-subscription-fees.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/subscription.svg" width="30px" />
+                                        <img src="/imgs/subscription.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Subscription Fees
                                         </span>
                                     </Link>
                                     <Link to="bills-maintenance.html" class="btn btn-dashboardicon btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/settings.svg" width="30px" />
+                                        <img src="/imgs/settings.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                         Maintenance
                                         </span>
                                     </Link>
                                     <Link to="bills-water.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/water-drop.svg" width="30px" />
+                                        <img src="/imgs/water-drop.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Water
                                         </span>
                                     </Link>
                                     <Link to="bills-electricity.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/electricity.svg" width="30px" />
+                                        <img src="/imgs/electricity.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Electricity
                                         </span>
                                     </Link>
                                     <Link to="bills-sewage.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/sewage.svg" width="30px" />
+                                        <img src="/imgs/sewage.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Sewage
                                         </span>
                                     </Link>
                                     <Link to="bills-internet.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/wifi.svg" width="30px" />
+                                        <img src="/imgs/wifi.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Internet
                                         </span>
                                     </Link>
                                     <Link to="bills-cabletv.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/tv.svg" width="30px" />
+                                        <img src="/imgs/tv.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Cabletv
                                         </span>
                                     </Link>
                                     <Link to="bills-gas.html" class="btn btn-dashboardicon  btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/gas.svg" width="30px" />
+                                        <img src="/imgs/gas.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Gas
@@ -132,70 +132,70 @@ const QuickLinks = () => {
                             <div class="row text-left">
                                 <div class="col-12 align-self-center mt-4">
                                     <Link to="info-tenancy.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/family.svg" width="30px" />
+                                        <img src="/imgs/family.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Tenancy
                                         </span>
                                     </Link>
                                     <Link to="info-utilities.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/utilities.svg" width="30px" />
+                                        <img src="/imgs/utilities.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Utilities
                                         </span>
                                     </Link>
                                     <Link to="info-insurance.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/insurance.svg" width="30px" />
+                                        <img src="/imgs/insurance.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Insurance
                                         </span>
                                     </Link>
                                     <Link to="info-building.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/company.svg" width="30px" />
+                                        <img src="/imgs/company.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Building
                                         </span>
                                     </Link>
                                     <Link to="info-unit.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/tenant.svg" width="30px" />
+                                        <img src="/imgs/tenant.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Unit
                                         </span>
                                     </Link>
                                     <Link to="info-servicecharge.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/money-bag.svg" width="30px" />
+                                        <img src="/imgs/money-bag.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Service Charge
                                         </span>
                                     </Link>
                                     <Link to="info-rate-taxes.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/pie-chart.svg" width="30px" />
+                                        <img src="/imgs/pie-chart.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Rate & Taxes
                                         </span>
                                     </Link>
                                     <Link to="info-assessmentrate.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img img src="/imgs/assessment.svg" width="24px" />
+                                        <img img src="/imgs/assessment.svg" width="24px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Assessment Rate
                                         </span>
                                     </Link>
                                     <Link to="info-quitrent.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/quitrent.svg" width="24px" />
+                                        <img src="/imgs/quitrent.svg" width="24px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             Quit Rent
                                         </span>
                                     </Link>
                                     <Link to="info-user-guide.html" class="btn btn-dashboardicon current btn-default width160 btn-lg m-2">
-                                        <img src="/imgs/guide.svg" width="30px" />
+                                        <img src="/imgs/guide.svg" width="30px" alt="" />
                                         <br />
                                         <span class="mt-5 font-light">
                                             User Manual
