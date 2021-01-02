689b94a38d5b4d4931a2956c7021d2a9a1c1a551
ayham <ayhamsysy@gmail.com>
2021-01-03 02:01:29 +0800

    remove unwanted files

--- src/App.js
+++ src/App.js
@@ -13,9 +13,8 @@ import "./assets/css/chartist.css";
 import "lightgallery.js/dist/css/lightgallery.css"; //DISCUSS what this for
 
 //pages
-import YardCondition from "./pages/Landlord/YardCondition";
+
 import ViewingDetails from "./pages/Landlord/ViewingDetails";
-import UtilityCondition from "./pages/Landlord/UtilityCondition";
 import UtilitiesOf from "./pages/Landlord/UtilitiesOf";
 import Utilities from "./pages/Landlord/Utilities";
 import UnitViewings from "./pages/Landlord/UnitViewings";
@@ -33,8 +32,7 @@ import Payables from "./pages/Landlord/Payables";
 import Offers from "./pages/Landlord/Offers";
 import OfferDetails from "./pages/Landlord/OfferDetails";
 import Notifications from "./pages/Landlord/Notifications.js";
-import MBedCondition from "./pages/Landlord/MBedCondition";
-import MBathCondition from "./pages/Landlord/MBathCondition";
+
 import MaintenanceDetails from "./pages/Landlord/MaintenanceDetails";
 import MaintenanceChatRequest from "./pages/Landlord/MaintenanceChatRequest";
 import MaintenanceChatPayment from "./pages/Landlord/MaintenanceChatPayment";
@@ -42,52 +40,19 @@ import MaintenanceChatInteraction from "./pages/Landlord/MaintenanceChatInteract
 import Maintenance from "./pages/Landlord/Maintenance";
 
 import Login from "./pages/Landlord/Login";
-import LivingCondition from "./pages/Landlord/LivingCondition";
-import KitchenCondition from "./pages/Landlord/KitchenCondition";
-import InventoryWater from "./pages/Landlord/InventoryWater";
-import InventoryVehicle from "./pages/Landlord/InventoryVehicle";
-import InventoryRemoteControl from "./pages/Landlord/InventoryRemoteControl";
-import UnitInventories from "./pages/Landlord/UnitInventories";
-import InventoryKey from "./pages/Landlord/InventoryKey";
-import InventoryGas from "./pages/Landlord/InventoryGas";
-import InventoryYard from "./pages/Landlord/InventoryYard";
-import InventoryBalcony from "./pages/Landlord/InventoryBalcony";
-import InventoryKitchen from "./pages/Landlord/InventoryKitchen";
-import InventoryEntrance from "./pages/Landlord/InventoryEntrance";
-import InventoryLiving from "./pages/Landlord/InventoryLiving";
-import InventoryDining from "./pages/Landlord/InventoryDining";
-import InventoryUtility from "./pages/Landlord/InventoryUtility";
-import InventoryElectricity from "./pages/Landlord/InventoryElectricity";
-import InventoryBeds from "./pages/Landlord/InventoryBeds";
-import InventoryBath from "./pages/Landlord/InventoryBath";
-import InventoryMBath from "./pages/Landlord/InventoryMBath";
-import InventoryCBath1 from "./pages/Landlord/InventoryCBath1";
-import InventoryCBath2 from "./pages/Landlord/InventoryCBath2";
-import InventoryCBath3 from "./pages/Landlord/InventoryCBath3";
-import InventoryMBed from "./pages/Landlord/InventoryMBed";
-import InventoryCBed1 from "./pages/Landlord/InventoryCBed1";
-import InventoryCBed2 from "./pages/Landlord/InventoryCBed2";
-import InventoryCBed3 from "./pages/Landlord/InventoryCBed3";
-import InventoryAccessCard from "./pages/Landlord/InventoryAccessCard";
 import Financials from "./pages/Landlord/Financials";
-import EntranceCondition from "./pages/Landlord/EntranceCondition";
-import DiningCondition from "./pages/Landlord/DiningCondition";
 import Dashboard from "./pages/Landlord/Dashboard";
-import CommonBed3Condition from "./pages/Landlord/CommonBed3Condition";
-import CommonBed2Condition from "./pages/Landlord/CommonBed2Condition";
-import CommonBed1Condition from "./pages/Landlord/CommonBed1Condition";
-import CommonBath3Condition from "./pages/Landlord/CommonBath3Condition";
-import CommonBath2Condition from "./pages/Landlord/CommonBath2Condition";
-import CommonBath1Condition from "./pages/Landlord/CommonBath1Condition";
+import EntranceCondition from "./pages/Landlord/EntranceCondition";
+import UnitInventories from "./pages/Landlord/UnitInventories";
+ 
+
 import CheckInLeaflet from "./pages/Landlord/CheckInLeaflet";
 import UnitKitsCats from "./pages/Landlord/UnitKitsCats";
 import UnitCheckInOut from "./pages/Landlord/UnitCheckInOut";
 import Calendars from "./pages/Landlord/Calendars";
 import Bills from "./pages/Landlord/Bills";
 import BillOf from "./pages/Landlord/BillsOf";
-import BedsCondition from "./pages/Landlord/BedsCondition";
-import BathsCondition from "./pages/Landlord/BathsCondition";
-import BalconyCondition from "./pages/Landlord/BalconyCondition";
+
 import Activity from "./pages/Landlord/Activity.js";
 import Logout from "./pages/Landlord/Logout";
 import { AppProvider } from "./context/settings";
@@ -172,127 +137,6 @@ export default function App() {
             <Route path="/landlord/UnitMeters/:meterName">
               <StandardPage>
                 <UnitMeters siteMap={"Unit meters"} />
-              </StandardPage>
-            </Route>
-
-            <Route path="/landlord/inventoryaccesscard">
-              <StandardPage>
-                <InventoryAccessCard siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryentrance">
-              <StandardPage>
-                <InventoryEntrance siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorywater">
-              <StandardPage>
-                <InventoryWater siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryutility">
-              <StandardPage>
-                <InventoryUtility siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorykitchen">
-              <StandardPage>
-                <InventoryKitchen siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorydining">
-              <StandardPage>
-                <InventoryDining siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryyard">
-              <StandardPage>
-                <InventoryYard siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorybalcony">
-              <StandardPage>
-                <InventoryBalcony siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryliving">
-              <StandardPage>
-                <InventoryLiving siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorygas">
-              <StandardPage>
-                <InventoryGas siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryelectricity">
-              <StandardPage>
-                <InventoryElectricity siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryremotecontrol">
-              <StandardPage>
-                <InventoryRemoteControl siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventoryvehicle">
-              <StandardPage>
-                <InventoryVehicle siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorybeds">
-              <StandardPage>
-                <InventoryBeds siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorybath">
-              <StandardPage>
-                <InventoryBath siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryMBath">
-              <StandardPage>
-                <InventoryMBath siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryCBath1">
-              <StandardPage>
-                <InventoryCBath1 siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryCBath2">
-              <StandardPage>
-                <InventoryCBath2 siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryCBath3">
-              <StandardPage>
-                <InventoryCBath3 siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryMBed">
-              <StandardPage>
-                <InventoryMBed siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryCBed1">
-              <StandardPage>
-                <InventoryCBed1 siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryCBed2">
-              <StandardPage>
-                <InventoryCBed2 siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/InventoryCBed3">
-              <StandardPage>
-                <InventoryCBed3 siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/inventorykey">
-              <StandardPage>
-                <InventoryKey siteMap={""} />
               </StandardPage>
             </Route>
             <Route path="/quicklinks/:cellId">
@@ -420,86 +264,7 @@ export default function App() {
                 <EntranceCondition siteMap={""} />
               </StandardPage>
             </Route>
-            <Route path="/landlord/KitchenCondition">
-              <StandardPage>
-                <KitchenCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/LivingCondition">
-              <StandardPage>
-                <LivingCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/DiningCondition">
-              <StandardPage>
-                <DiningCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/YardCondition">
-              <StandardPage>
-                <YardCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/BalconyCondition">
-              <StandardPage>
-                <BalconyCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/UtilityCondition">
-              <StandardPage>
-                <UtilityCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/BedsCondition">
-              <StandardPage>
-                <BedsCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/MBedCondition">
-              <StandardPage>
-                <MBedCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/CommonBed1Condition">
-              <StandardPage>
-                <CommonBed1Condition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/CommonBed2Condition">
-              <StandardPage>
-                <CommonBed2Condition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/CommonBed3Condition">
-              <StandardPage>
-                <CommonBed3Condition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/BathsCondition">
-              <StandardPage>
-                <BathsCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/MBathCondition">
-              <StandardPage>
-                <MBathCondition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/CommonBath1Condition">
-              <StandardPage>
-                <CommonBath1Condition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/CommonBath2Condition">
-              <StandardPage>
-                <CommonBath2Condition siteMap={""} />
-              </StandardPage>
-            </Route>
-            <Route path="/landlord/CommonBath3Condition">
-              <StandardPage>
-                <CommonBath3Condition siteMap={""} />
-              </StandardPage>
-            </Route>
+          
             <Route path="/landlord/UnitCheckInOut">
               <StandardPage>
                 <UnitCheckInOut siteMap={""} />
---
+++ src/del.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm ./pages/Landlord/YardCondition.js
+rm ./pages/Landlord/UtilityCondition.js
+rm ./pages/Landlord/MBedCondition.js
+rm ./pages/Landlord/MBathCondition.js
+rm ./pages/Landlord/LivingCondition.js
+rm ./pages/Landlord/KitchenCondition.js
+rm ./pages/Landlord/UnitInventories.js
+rm ./pages/Landlord/DiningCondition.js
+rm ./pages/Landlord/CommonBed3Condition.js
+rm ./pages/Landlord/CommonBed2Condition.js
+rm ./pages/Landlord/CommonBed1Condition.js
+rm ./pages/Landlord/CommonBath3Condition.js
+rm ./pages/Landlord/CommonBath2Condition.js
+rm ./pages/Landlord/CommonBath1Condition.js
+rm ./pages/Landlord/BedsCondition.js
+rm ./pages/Landlord/BathsCondition.js
+rm ./pages/Landlord/BalconyCondition.js
